c031d82fde8843bdfa7bf4de6d9defed2bdea413
Jesse Shawl <jesse@jshawl.com>
2016-07-28 10:54:47 -0400

    update schedule

--- update-yamlandar.sh
+++ update-yamlandar.sh
@@ -4,14 +4,13 @@ git clone git@github.com:ga-dc/wdi-schedule.git -b gh-pages
 mv basic.ics wdi-schedule/wdi_cal.ics
 cd wdi-schedule
 ruby ical.rb > sk.yml
-echo '- "start-date": "2016-05-19"' > new.yml
+echo '- "start-date": "2016-05-16"' > new.yml
 echo "- days:" >> new.yml
 cd ../
 cat wdi-schedule/new.yml
 cat wdi-schedule/new.yml > schedule.yml
 cat wdi-schedule/sk.yml >> schedule.yml
 yamlandar -c > index.html
-rm -rf wdi-schedule 
 git add .
 git commit -m "update schedule"
 git push origin gh-pages
