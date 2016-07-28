45d274879c769d2da45adb48cb4ba2db5453afe2
Jesse Shawl <jesse@jshawl.com>
2016-07-28 09:44:08 -0400

    update schedule

---
+++ update-yamlandar.sh
@@ -0,0 +1,17 @@
+rm -rf wdi-schedule
+curl -O https://calendar.google.com/calendar/ical/generalassemb.ly_11ecttiuoidin7hcvigui7cdg4%40group.calendar.google.com/public/basic.ics
+git clone git@github.com:ga-dc/wdi-schedule.git -b gh-pages
+mv basic.ics wdi-schedule/wdi_cal.ics
+cd wdi-schedule
+ruby ical.rb > sk.yml
+echo '- "start-date": "2016-05-23"' > new.yml
+echo "- days:" >> new.yml
+cd ../
+cat wdi-schedule/new.yml
+cat wdi-schedule/new.yml > schedule.yml
+cat wdi-schedule/sk.yml >> schedule.yml
+yamlandar -c > index.html
+rm -rf wdi-schedule 
+git add .
+git commit -m "update schedule"
+git push origin gh-pages
